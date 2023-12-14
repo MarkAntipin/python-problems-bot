7e1a32852ea74412bc260555c851897edded8a0d
Kkkasum <kasum2605@mail.ru>
2023-12-15 02:15:00 +0400

    add functional tests ci

--- tests_functional/conftest.py
+++ tests_functional/conftest.py
@@ -30,9 +30,9 @@ async def pg_fixture() -> asyncpg.Connection:
 
 @pytest.fixture(autouse=True)
 async def env(monkeypatch: pytest.MonkeyPatch) -> None:
-    monkeypatch.setenv('HOST', 'localhost')
-    monkeypatch.setenv('USER', 'python-problems-bot')
-    monkeypatch.setenv('PASSWORD', 'python-problems-bot')
-    monkeypatch.setenv('DATABASE', 'python-problems-bot')
-    monkeypatch.setenv('PORT', '5432')
+    monkeypatch.setenv('TEST_PG_HOST', 'localhost')
+    monkeypatch.setenv('TEST_PG_USER', 'python-problems-bot')
+    monkeypatch.setenv('TEST_PG_PASSWORD', 'python-problems-bot')
+    monkeypatch.setenv('TEST_PG_DATABASE', 'python-problems-bot')
+    monkeypatch.setenv('TEST_PG_PORT', '5436')
     monkeypatch.setenv('TOKEN', '')
