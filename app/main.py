dfb717dcbcefda635db5f9c4c5367027fe034d17
Kkkasum <kasum2605@mail.ru>
2024-01-05 13:05:02 +0400

    few changes

--- app/main.py
+++ app/main.py
@@ -34,5 +34,16 @@ async def get_question_page(request: Request, question_id: int): # noqa ANN201
     )
 
 
+@app.post('/question/{question_id:int}/result')
+async def execute_code(body: str = Body()):
+    compiled_code = compile(body, __name__, 'exec')
+    print(exec(compiled_code))
+
+    return {
+        "status": "success",
+        "data": 'result'
+    }
+
+
 if __name__ == '__main__':
     uvicorn.run(app)
