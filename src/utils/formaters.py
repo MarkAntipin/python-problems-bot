b0fe54d99e929056617d24cc8c4e73f5a05bbc29
Mark Antipin <markantipin@Marks-MacBook-Pro.local>
2024-03-26 09:51:52 +0100

    html -> markdown

--- src/utils/formaters.py
+++ src/utils/formaters.py
@@ -5,7 +5,7 @@ from src.texts import CORRECT_ANSWER_TEXT, INCORRECT_ANSWER_TEXT
 
 
 def _format_choices(choices: dict) -> str:
-    return '\n'.join([f'{key.upper()}) {value}' for key, value in choices.items()])
+    return '\n'.join([f'*{key.upper()}\)* {value}' for key, value in choices.items()])
 
 
 def format_question(question: Question) -> str:
@@ -30,9 +30,11 @@ def format_explanation(question: Question, is_correct: bool) -> str:
 
 
 def format_advice(advice: Advice) -> str:
-    formatted_advice = f'Я понял, что тебе стоит подтянуть тему "{advice.theme}".\n' \
-                       f'Вот <a href="{advice.link}">ссылка</a>\n' \
-                       f'Прочти, чтобы стать еще круче!'
+    formatted_advice = (
+        f'Я понял, что тебе стоит подтянуть тему "{advice.theme}".\n'
+        f'Вот [ссылка]({advice.link})'
+        'Прочти, чтобы стать еще круче!'
+    )
 
     return formatted_advice
 
@@ -51,18 +53,18 @@ def format_word_declensions(n: int, declensions: dict[str, str]) -> str:
 
 
 def format_leaders_message(leaders: list[Leader], user_in_leaders: UserInLeaders | None) -> str:
-    message_text = '<b>Таблица лидеров:</b>\n'
+    message_text = '*Таблица лидеров:*\n'
     score_declensions = {'accusative_many': 'баллов', 'accusative_solo': 'балла', 'nominative': 'балл'}
 
     for i, leader in enumerate(leaders, start=1):
         score_word = format_word_declensions(n=leader.score, declensions=score_declensions)
-        user_link = f'<a href="https://t.me/{leader.username}">{leader.first_name}</a>'
+        user_link = f'[{leader.first_name}](https://t.me/{leader.username})'
         message_text += f'{i}. {user_link} - {leader.score} {score_word}\n'
 
     if user_in_leaders:
         score_word = format_word_declensions(n=user_in_leaders.score, declensions=score_declensions)
         message_text += (
-            f'\n<b>Ваше текущее место:</b> {user_in_leaders.position}.'
+            f'\n*Ваше текущее место:* {user_in_leaders.position}.'
             f' Вы набрали {user_in_leaders.score} {score_word}.'
         )
 
