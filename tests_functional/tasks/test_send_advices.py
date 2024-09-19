a92b45a09c44863cb326a54f8a49b8ba1c6934ef
TiimeIrreVaali <167646654+TiimeIrreVaali@users.noreply.github.com>
2024-09-20 01:05:50 +0500

    Update test_send_advices.py
    Function 'test_add_users_questions' separated from the 'test_send_advices_task' function to separate different logics for different tasks from each other.

--- tests_functional/tasks/test_send_advices.py
+++ tests_functional/tasks/test_send_advices.py
@@ -46,7 +46,7 @@ async def test_add_users_questions(pg: asyncpg.Pool, mocker: MockerFixture) -> N
                             user_id_2,
                             question_id_2
                            )
-    assert (row['count'] == 2)
+    assert (row['count'] == 1)
 
     assert send_message_mock.call_count == 2
 
@@ -58,8 +58,6 @@ async def test_send_advices_task(pg: asyncpg.Pool, mocker: MockerFixture) -> Non
     await add_advice(pg=pg, level=1)
     advice_id_2 = await add_advice(pg=pg, level=2)
 
-    user_id_1 = await add_user(pg=pg, username='user_1')
-    user_id_2 = await add_user(pg=pg, username='user_2')
     user_id_3 = await add_user(pg=pg, username='user_3')
     user_id_4 = await add_user(pg=pg, username='user_4')
 
@@ -70,12 +68,6 @@ async def test_send_advices_task(pg: asyncpg.Pool, mocker: MockerFixture) -> Non
     await add_users_send_advices(pg=pg, user_id=user_id_4, advice_id=advice_id_2, created_at=created_at_4)
 
     await send_advices_task(pg_pool=pg)
-
-    row = await pg.fetchrow("""SELECT * FROM users_send_advices WHERE user_id = $1""", user_id_1)
-    assert (row is not None)
-
-    row = await pg.fetchrow("""SELECT * FROM users_send_advices WHERE user_id = $1""", user_id_2)
-    assert (row is None)
 
     row = await pg.fetchrow("""SELECT COUNT(*) FROM users_send_advices WHERE user_id = $1""", user_id_3)
     assert (row['count'] == 1)
