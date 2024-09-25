8f23dfaf670adb5a5b46a492ae0f68a96b06b2f6
TiimeIrreVaali <mirriy@mail.ru>
2024-09-25 21:25:26 +0500

    Some edits to test_send_advices, adding some local tests: - test_send_advice_task__short_time__not_send_advice - test for not sending an advice, if the interval after the last advice is too short; - test_send_advice_task__no_weak_theme - test for checking the absence of weak themes; - test_send_advice_task__no_weak_theme__not_send - test for checking that advices are not sent if a user has no weak themes; - test_send_advice_task__long_time__send_advice - test for sending a weak-theme-advice, if the interval after the last advice is long enough.

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
-from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+
+from dotenv import load_dotenv
 
 
 BASE_DIR = Path(__file__).resolve().parent
