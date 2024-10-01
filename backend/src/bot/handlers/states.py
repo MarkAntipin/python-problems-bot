4a4786f59c892c2d0d1f466f63f91d485996d1c6
Mark Antipin <markantipin@Marks-MacBook-Pro.local>
2024-10-01 09:49:02 +0200

    add payment in handler

--- backend/src/bot/handlers/states.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum, auto
-
-
-class States(StrEnum):
-    daily_question = auto()
-    onboarding = auto()
-    change_level = auto()
-    finish_onboarding = auto()
-    code = auto()
